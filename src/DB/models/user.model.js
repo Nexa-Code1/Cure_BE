38cf4c429f0448be310e7a4a374d9f0735a5fd6e
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-10-20 17:20:25 +0300

    Fix some issues

--- src/DB/models/user.model.js
+++ src/DB/models/user.model.js
@@ -1,6 +1,7 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../connection.js";
 import bcrypt from "bcrypt";
+
 class UserModel extends Model {
   toJSON() {
     const values = { ...this.get() };
--- src/Modules/User/Services/user.service.js
+++ src/Modules/User/Services/user.service.js
@@ -1,4 +1,5 @@
 import UserModel from "../../../DB/models/user.model.js";
+import { compareSync, hashSync } from "bcrypt";
 
 export const getMyProfile = async (req, res) => {
   try {
@@ -39,6 +40,16 @@ export const updatePassword = async (req, res) => {
   try {
     const { currentPassword, newPassword, confirmNewPassword } = req.body;
 
+    const user = await UserModel.unscoped().findByPk(req.user.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = compareSync(currentPassword, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid current password" });
+    }
+
     if (newPassword !== confirmNewPassword) {
       return res.status(400).json({ message: "Passwords do not match" });
     }
@@ -49,38 +60,39 @@ export const updatePassword = async (req, res) => {
       });
     }
 
-    const user = await UserModel.unscoped().findOne({
-      where: { id: req.user.id },
-    });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    const hashedPassword = hashSync(newPassword, 10);
 
-    const isPasswordValid = compareSync(currentPassword, user.password);
-    if (!isPasswordValid) {
-      return res.status(400).json({ message: "Invalid current password" });
-    }
+    await UserModel.update(
+      { password: hashedPassword },
+      { where: { id: req.user.id } }
+    );
 
-    await user.update({ password: newPassword });
-
-    return res.status(200).json({
-      message: "Password updated successfully",
-    });
+    return res.status(200).json({ message: "Password updated successfully" });
   } catch (error) {
     console.error("Update Password Error:", error);
-    return res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
   }
 };
 
 export const deleteMyProfile = async (req, res) => {
   try {
-    const user = await UserModel.findByPk(req.user.id);
+    const { password } = req.body;
+    const user = await UserModel.unscoped().findByPk(req.user.id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    await user.destroy();
+    const isPasswordValid = compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    await UserModel.destroy({ where: { id: req.user.id } });
     res.status(200).json({ message: "Profile deleted successfully" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
