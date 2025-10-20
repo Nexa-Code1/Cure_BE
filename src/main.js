0a3f8fea3243ffbb506af9065d028cb44f0f555f
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-10-20 23:53:33 +0300

    Try to host db and backend

--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ const bootstrap = async () => {
   routerHandler(app);
 
   app
-    .listen(3000, () => {
-      console.log("Server is running on port 3000");
+    .listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
     })
     .on("error", (error) => {
       console.error("Server error:", error);
