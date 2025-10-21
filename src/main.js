130c4f6800f471d29270af2b66eb9db99484f6d4
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-10-21 22:44:20 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cors from "cors";
 import connection from "./DB/connection.js";
 import routerHandler from "./Utils/router-handler.js";
 import { fileURLToPath } from "url";
@@ -15,6 +16,14 @@ const __dirname = path.dirname(__filename);
 const bootstrap = async () => {
   await connection();
   app.use(express.json());
+
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"],
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      credentials: true,
+    })
+  );
 
   app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
