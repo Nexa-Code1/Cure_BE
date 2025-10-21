e035fdc56b3fa7cb699b7c0bcc08913ff8d0e9dd
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-10-22 00:07:46 +0300

    Add Frontend host to cors

--- src/main.js
+++ src/main.js
@@ -19,7 +19,10 @@ const bootstrap = async () => {
 
   app.use(
     cors({
-      origin: ["http://localhost:5173"],
+      origin: [
+        "http://localhost:5173",
+        "https://onlinebookingdoctor.netlify.app/",
+      ],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
     })
