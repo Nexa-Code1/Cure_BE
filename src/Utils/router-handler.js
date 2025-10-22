f48b12c8af9b359d1f3a7b64109da99fbc119c21
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-10-23 01:34:49 +0300

    Fix router handler

--- src/Utils/router-handler.js
+++ src/Utils/router-handler.js
@@ -8,9 +8,6 @@ import userRouter from "../Modules/User/user.controller.js";
 import specialistsRouter from "../Modules/Specialists/specialists.controller.js";
 
 const routerHandler = (app) => {
-  app.use("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to Cure API" });
-  });
   app.use("/api/auth", authRouter);
   app.use("/api/user", userRouter);
   app.use("/api/doctor", doctorRouter);
@@ -18,6 +15,10 @@ const routerHandler = (app) => {
   app.use("/api/favourite", favRouter);
   app.use("/api/booking", bookingRouter);
   app.use("/api/specialists", specialistsRouter);
+
+  app.use("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to Cure API" });
+  });
 
   app.use(globalErrorHandler);
 };
