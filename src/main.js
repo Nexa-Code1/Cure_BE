d4c7c518fb9dc8e31b59ee6281a1ed0b979dc2c6
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-10-26 20:48:51 +0300

    update main

--- src/main.js
+++ src/main.js
@@ -18,9 +18,14 @@ const bootstrap = async () => {
 
   app.use(
     cors({
-      origin: "*",
+      origin: [
+        "http://localhost:5173",
+        "https://onlinebookingdoctor.netlify.app",
+        "https://curebe-production.up.railway.app/api",
+      ],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
     })
   );
 
